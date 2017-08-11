98ea5364357c8572a37f70cfd8ce5772ddee1465
conesgnomes <connorlarsen@gmail.com>
2017-08-10 17:47:41 -0700

    begin styling mobile view on landing page and continue to work on mobile nav animation

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -6,6 +6,7 @@ $(function() {
 		$(this).toggleClass('open');
     $('.nav-mobile').toggleClass('open');
     $('.mobile-nav-content').toggleClass('open');
+    $('.mobile-nav-text').toggleClass('open');
     $('.nav-span').toggleClass('open');
 	});
 
